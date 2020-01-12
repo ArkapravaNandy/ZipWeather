1ad89ee3bb32941f63980561032ed77420111be5
ArkapravaNandy <arkapravanandy2000@gmail.com>
2020-01-12 22:47:15 +0530

    linked data from AirNow API using JSON file

--- ZipWeather/views.py
+++ ZipWeather/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # This is my views.py file
 def home(request):
-    return render(request, 'Index.html', {})    
+    import json
+    import requests
+
+    api_request=requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=DF8CE76E-D834-4728-9948-CB7B2AE0CB8E")
+    
+    try:
+        api=json.loads(api_request.content)
+    except Exception as e:
+        api="Error 404...not found!"
+
+    return render(request, 'Index.html', {'api' : api})    
